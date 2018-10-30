f13de8ab8e91c2879fe576f4d8669cf6c832d320
suhyun <suhyun__-@nate.com>
2018-10-30 19:37:35 +0900

    10월30일 스터디

---
+++ Study02/Study02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study02
+//
+//  Created by 김수현 on 2018. 10. 30..
+//  Copyright © 2018년 김수현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
